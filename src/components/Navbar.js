1e422a07c0a887e050495b47b5aad8aa58c6e48c
Vllaadd <vlladdiimiir@gmail.com>
2021-06-20 03:19:15 -0400

    logo is working now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css';
 import {Button}from './Button.js'
@@ -17,13 +17,17 @@ function Navbar() {
             setButton(true);
         }
     };
+//we are using useEffect for sign up button not showing up after we resize the page
+    useEffect(() => {
+        showButton();
+    }, []);
 
     window.addEventListener('resize', showButton);
 
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     TRVL <i className='fab fa-typo3'></i>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
