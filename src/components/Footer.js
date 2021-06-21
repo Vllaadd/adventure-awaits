7b9b6cbe880cc088fc0a8fef48f095820409a92f
Vllaadd <vlladdiimiir@gmail.com>
2021-06-21 11:22:40 -0400

    creating four lines of links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,27 @@ function Footer(){
                 <div>
                     <form>
                         <input name='email' type='email' placeholder='Your Email'/>
-                        <Button buttonStyle='btn--outline'></Button>
+                        <Button buttonStyle='btn--outline'>Subscribe</Button>
                     </form>
                 </div>
             </section>
             <section className='footer-links'>
-
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
+                    </div>
+                    <div className='footer-link-items'>
+                        <h2>Contact Us</h2>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>Videos</h2>
+                    </div>
+                    <div className='footer-link-items'>
+                        <h2>Social Media</h2>
+                    </div>
+                </div>
             </section>
             <section className='social-media'>
 
