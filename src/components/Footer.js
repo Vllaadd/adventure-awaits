1b65ebce13dbdb0550c4e7b0537659654ce7e49c
Vllaadd <vlladdiimiir@gmail.com>
2021-06-21 11:29:06 -0400

    links done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,17 +20,37 @@ function Footer(){
                 <div className='footer-link-wrapper'>
                     <div className='footer-link-items'>
                         <h2>About Us</h2>
+                        <Link to='/'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
                     </div>
                     <div className='footer-link-items'>
                         <h2>Contact Us</h2>
+                        <Link to='/'>Contact Us</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                        <Link to='/'>Marketing Team</Link>
                     </div>
                 </div>
                 <div className='footer-link-wrapper'>
                     <div className='footer-link-items'>
                         <h2>Videos</h2>
+                        <Link to='/'>Submit a video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                        <Link to='/'>Social Networks</Link>
                     </div>
                     <div className='footer-link-items'>
                         <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>TikTok</Link>
+                        <Link to='/'>Snapchat</Link>
+                        <Link to='/'>Twitter</Link>
                     </div>
                 </div>
             </section>
